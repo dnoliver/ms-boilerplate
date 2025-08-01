e277499f1bb465bca1441465aef2985dddd6ee92
Nicolas Oliver <nicolasoliver03@gmail.com>
2025-08-01 20:26:57 +0000

    feat: enhance CI workflow with unit tests and add security linters

--- scripts.py
+++ scripts.py
@@ -1,9 +1,25 @@
 import subprocess
 
 
-def test():
+def pylint():
     """
-    Run all unittests. Equivalent to:
+    Run pylint linter. Equivalent to:
+    `poetry run pylint . --ignore .venv`
+    """
+    subprocess.run(["pylint", ".", "--ignore", ".venv"], check=True)
+
+
+def bandit():
+    """
+    Run bandit security linter. Equivalent to:
+    `poetry run bandit -r . --exclude .venv`
+    """
+    subprocess.run(["bandit", "-r", ".", "--exclude", "./.venv"], check=True)
+
+
+def unittest():
+    """
+    Run all unit tests. Equivalent to:
     `poetry run python -u -m unittest discover -v -s ./test -p test_*.py`
     """
     subprocess.run(
